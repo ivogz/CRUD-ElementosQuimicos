b966b340d4bc4460509cab0e9aa65beac09725c5
Ivan Gonzalez <ivogz.01@gmail.com>
2024-05-22 18:25:09 -0300

    clases

--- Entidades/Elemento.cs
+++ Entidades/Elemento.cs
@@ -2,6 +2,17 @@
 {
     public class Elemento
     {
+        protected int nAtomico;
+        protected string nombre;
+        protected string simbolo;
+        protected int grupo;
+        protected int periodo;
+
+
+
+
+
+
 
     }
 }
