c512d07d6ab01c6d09182df929ba03917f30bc98
Ivan Gonzalez <ivogz.01@gmail.com>
2024-05-22 21:09:28 -0300

    Sobrecargas operadores, equals, gethashcode en clase padre

--- Entidades/Elemento.cs
+++ Entidades/Elemento.cs
@@ -9,6 +9,11 @@
         protected int periodo;
         protected double masaAtomica;
 
+
+
+
+
+
         public Elemento(int nAtomico, string nombre, string simbolo, int grupo, int periodo, double masaAtomica)
         {
             this.nAtomico = nAtomico;
@@ -21,5 +26,50 @@
 
 
 
+
+
+        // SOBRECARGAS OPERADORES, EQUALS Y GETHASHCODE
+        public static bool operator ==(Elemento e, Elemento e1)
+        {
+            if (ReferenceEquals(e, e1))
+            {
+                return true;
+            }
+
+            if (e is null || e1 is null)
+            {
+                return false;
+            }
+
+            return e.nAtomico == e1.nAtomico || e.nombre == e1.nombre || e.simbolo == e1.simbolo || (e.grupo == e1.grupo && e.periodo == e1.periodo);
+        }
+
+        public static bool operator !=(Elemento e, Elemento e1)
+        {
+            return !(e == e1);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Elemento)
+            {
+                return ((Elemento)obj == this);
+            }
+            else { return false;}
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(nAtomico, nombre, simbolo, grupo, periodo);
+        }
+
+
+
+
+
+
+
     }
+
+ 
 }
